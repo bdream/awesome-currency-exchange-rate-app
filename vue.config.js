e72c2e0b20002b21f82699795b0cb90964d51ade
bdream <bdletterbox@gmail.com>
2022-06-10 23:00:07 +0300

    Try to fix 404 errors due Vue app run

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/awesome-currency-exchange-rate-app/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "./awesome-currency-exchange-rate-app/" : "/",
 })
